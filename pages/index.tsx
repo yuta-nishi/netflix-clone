e5bbc3e879ceeab4a77ee01e597cad615037f204
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-10 17:43:47 +0900

    feat: initialize next.js

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-2xl text-green-500">Netflix clone</h1>
+    </>
+  );
+};
+
+export default HomePage;
